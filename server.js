92852f0cda473cde337d0606abbd6e3ace2ea816
Oumou Doumbia <doumbia.oumou@gmail.com>
2023-05-03 09:51:27 -0400

    Create sweet message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4200
 
 app.get('/', (req, res) => {
-  res.send('Bonjour le monde! Faites attention.')
+  res.send('Bon matin! Good success!')
 })
 
 app.listen(port, () => {
